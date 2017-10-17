904c44f9c65a3b65d4d163b19cb86409fef552d2
TrongMTran <trminhtrong@gmail.com>
2017-10-17 17:37:25 +0700

    update port

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ var path = require('path')
 var app = express();
 app.use(express.static(path.resolve(__dirname, "www")));
 app.set('port', process.env.PORT || 3000);
-app.listen(app.get('port'), function() {
- console.log('listening to Port', app.get('port'));
-});
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
